b82cf53726ad37ea5250ddfb3cb62dd0d4c427bd
occvlt <occvlt@yandex.ru>
2019-02-01 17:55:17 +0400

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Alexander L on 01/02/2019.
+//  Copyright © 2019 Alexander Lyalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func showAllert() {
+        print("Hello!")
+    }
+}
+
