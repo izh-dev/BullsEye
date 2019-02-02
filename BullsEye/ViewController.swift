73ff51109e68499ba2870305399a70e1ce1add92
Alexander <occvlt@yandex.ru>
2019-02-02 18:19:33 +0400

    add total score and round functionality

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,29 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var sliderCurrentValue: Int = 0
-    var targetValue: Int = 0
+    var sliderCurrentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
         sliderCurrentValue = Int(roundedValue)
+        startNewRound()
     }
 
     
     @IBAction func showAlert() {
-        let message = """
-        The value of the slider is now: \(sliderCurrentValue)
-        The target value is: \(targetValue)
-        """
+        let diff = abs(sliderCurrentValue - targetValue)
+        let points = 100 - diff
+        
+        score += points
+        
+        let message = "You scored \(points) points"
         let alert = UIAlertController(title: "OK!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome!", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -41,8 +50,16 @@ class ViewController: UIViewController {
     
     func startNewRound() {
         targetValue = Int.random(in: 1...100)
+        round += 1
         sliderCurrentValue = 50
         slider.value = Float(sliderCurrentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
